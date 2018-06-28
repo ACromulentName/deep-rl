b14d65875f34a7979bdc6cdc9dbe1edae9db9ccb
Karthik Raveendran <karthik.raveendran@gmail.com>
2018-06-27 18:10:56 -0700

    Added train script

---
+++ keras-intro/train.py
@@ -0,0 +1,13 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
+def train():
+  print("Hi")
+
+def main():
+  train()
+
+if __name__ == "__main__":
+  main()
